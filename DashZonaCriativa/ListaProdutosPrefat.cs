920a61694f1d986d22045c2f1558bb0e7b681c6a
Roberto Guarize <guarizen@hotmail.com>
2020-01-09 21:08:27 -0500

    Dashboard-ZonaCriativa-Release5

---
+++ DashZonaCriativa/ListaProdutosPrefat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ListaProdutosPrefatAPI
+{
+    public partial class ListaProdutosPrefat
+    {
+        [JsonProperty("odata.metadata")]
+        public Uri OdataMetadata { get; set; }
+
+        [JsonProperty("odata.count")]
+        public long OdataCount { get; set; }
+
+        [JsonProperty("value")]
+        public Value[] Value { get; set; }
+    }
+
+    public partial class Value
+    {
+        [JsonProperty("produto_prefat")]
+        public long ProdutoPrefat { get; set; }
+
+        [JsonProperty("prefaturamento")]
+        public long Prefaturamento { get; set; }
+
+        [JsonProperty("produto")]
+        public long Produto { get; set; }
+
+        [JsonProperty("estampa")]
+        public long Estampa { get; set; }
+
+        [JsonProperty("cor")]
+        public long Cor { get; set; }
+
+        [JsonProperty("tamanho")]
+        public string Tamanho { get; set; }
+
+        [JsonProperty("quantidade")]
+        public long Quantidade { get; set; }
+
+        [JsonProperty("entregue")]
+        public long Entregue { get; set; }
+
+        [JsonProperty("saida")]
+        public object Saida { get; set; }
+    }
+
+    public partial class ListaProdutosPrefat
+    {
+        public static ListaProdutosPrefat FromJson(string json) => JsonConvert.DeserializeObject<ListaProdutosPrefat>(json, ListaProdutosPrefatAPI.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this ListaProdutosPrefat self) => JsonConvert.SerializeObject(self, ListaProdutosPrefatAPI.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
--- DashZonaCriativa/Program.cs
+++ DashZonaCriativa/Program.cs
@@ -5,6 +5,7 @@ using ListaPrecosAPI;
 using ListaPrefaturamentosAPI;
 using ListaProdutosAPI;
 using ListaProdutosEventosAPI;
+using ListaProdutosPrefatAPI;
 using ListaRepresentantesAPI;
 using ListaSaidasAPI;
 using MySql.Data.MySqlClient;
@@ -42,6 +43,7 @@ namespace DashZonaCriativa
             var ConnectMovEntradas = ConfigurationManager.AppSettings["ConnectMovEntradas"];
             var ConnectProdutosEventos = ConfigurationManager.AppSettings["ConnectProdutosEventos"];
             var ConnectPrefaturamentos = ConfigurationManager.AppSettings["ConnectPrefaturamentos"];
+            var ConnectProdutosPrefat = ConfigurationManager.AppSettings["ConnectProdutosPrefat"];
             var Authorization = ConfigurationManager.AppSettings["Authorization"];
 
             //Buscar Produtos e Incluir no Banco de dados MySQL.
@@ -980,10 +982,141 @@ namespace DashZonaCriativa
 
                                                                                         }
                                                                                     }
+                                                                                    //Buscar os Produtos Prefaturamento e Incluir no Banco de dados MySQL.
+                                                                                    try
+                                                                                    {
+                                                                                        MySqlConnection objConx09 = new MySqlConnection($"Server={ConnectMySQLDB};Database={DatabaseMySQLDB};Uid={UserMySQLDB};Pwd={PassMySQLDB}");
+                                                                                        objConx09.Open();
 
+                                                                                        var command09 = objConx09.CreateCommand();
+                                                                                        command09.CommandText = "SELECT COUNT(*) AS N FROM PRODUTO_PREFAT";
+                                                                                        var retCommnad9 = command09.ExecuteReaderAsync();
+                                                                                        string fault9 = retCommnad9.Status.ToString();
 
-                                                                                    //PROXIMO BLOCO!!!
+                                                                                        if (fault9 == "Faulted")
+                                                                                        {
+                                                                                            Console.WriteLine("Criando Tabela de Produto Prefaturamento...");
+                                                                                            var CommandInsert9 = objConx09.CreateCommand();
+                                                                                            CommandInsert9.CommandText = "CREATE TABLE PRODUTO_PREFAT (PRODUTO_PREFAT INT NOT NULL,PREFATURAMENTO INT,PRODUTO INT,ESTAMPA INT,COR INT,TAMANHO VARCHAR(5),QUANTIDADE INT,ENTREGUE INT,SAIDA INT);";
+                                                                                            CommandInsert9.ExecuteNonQuery();
+                                                                                            objConx09.Close();
+                                                                                        }
+                                                                                        objConx09.Close();
 
+                                                                                        var requisicaoWeb9 = WebRequest.CreateHttp($"{ConnectProdutosPrefat}" + "?$format=json");
+                                                                                        requisicaoWeb9.Method = "GET";
+                                                                                        requisicaoWeb9.Headers.Add("Authorization", $"{Authorization}");
+                                                                                        requisicaoWeb9.UserAgent = "RequisicaoAPIGET";
+                                                                                        requisicaoWeb9.Timeout = 1300000;
+                                                                                        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+                                                                                        Console.WriteLine("Listando e Incluindo Produtos Prefaturamentos...");
+                                                                                        using (var resposta9 = requisicaoWeb9.GetResponse())
+                                                                                        {
+                                                                                            var streamDados9 = resposta9.GetResponseStream();
+                                                                                            StreamReader reader9 = new StreamReader(streamDados9);
+                                                                                            object objResponse9 = reader9.ReadToEnd();
+                                                                                            var statusCodigo9 = ((System.Net.HttpWebResponse)resposta9).StatusCode;
+
+                                                                                            ListaProdutosPrefat ppft = JsonConvert.DeserializeObject<ListaProdutosPrefat>(objResponse9.ToString());
+
+                                                                                            int pppref = 0;
+                                                                                            foreach (var pptf in ppft.Value)
+                                                                                            {
+                                                                                                try
+                                                                                                {
+                                                                                                    MySqlConnection objConx9 = new MySqlConnection($"Server={ConnectMySQLDB};Database={DatabaseMySQLDB};Uid={UserMySQLDB};Pwd={PassMySQLDB}");
+                                                                                                    objConx9.Open();
+
+                                                                                                    while (pppref < 1)
+                                                                                                    {
+                                                                                                        if (pppref == 1)
+                                                                                                            break;
+                                                                                                        var commandelet = objConx9.CreateCommand();
+                                                                                                        commandelet.CommandText = "DELETE FROM PRODUTO_PREFAT";
+                                                                                                        commandelet.ExecuteNonQuery();
+                                                                                                        pppref++;
+                                                                                                    }
+
+                                                                                                    var command9 = objConx9.CreateCommand();
+                                                                                                    command9.CommandText = "INSERT INTO PRODUTO_PREFAT (PRODUTO_PREFAT,PREFATURAMENTO,PRODUTO,ESTAMPA,COR,TAMANHO,QUANTIDADE,ENTREGUE,SAIDA)" +
+                                                                                                                                $"VALUES({pptf.ProdutoPrefat}," + $"{pptf.Prefaturamento}," + $"{pptf.Produto}," + $"{pptf.Estampa}," + $"{pptf.Cor}," + $"\"{pptf.Tamanho}\","
+                                                                                                                                + $"{pptf.Quantidade}," + $"{pptf.Entregue}," + $"{pptf.Saida}" + ")";
+
+                                                                                                    command9.ExecuteNonQuery();
+                                                                                                    objConx9.Close();
+
+                                                                                                }
+                                                                                                catch (MySqlException etv)
+                                                                                                {
+                                                                                                    Console.WriteLine($"ID Produto Prefat :  {pptf.ProdutoPrefat} - Prefaturamento: {pptf.Prefaturamento} - Erro: {etv.Message}");
+
+                                                                                                    //Verificando a pasta de Log. 
+                                                                                                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                                                                                                    string path = @"C:\Dashboard-Log\" + DateTimeOffset.Now.ToString("ddMMyyyy") + ".log";
+
+                                                                                                    //Verifica se o arquivo de Log não existe e inclui as informações.
+                                                                                                    if (!File.Exists(path))
+                                                                                                    {
+                                                                                                        DirectoryInfo dir = new DirectoryInfo(FileLog);
+
+                                                                                                        foreach (FileInfo fi in dir.GetFiles())
+                                                                                                        {
+                                                                                                            fi.Delete();
+                                                                                                        }
+
+                                                                                                        string nomeArquivo1 = @"C:\Dashboard-Log\" + DateTimeOffset.Now.ToString("ddMMyyyy") + ".log";
+                                                                                                        StreamWriter writer1 = new StreamWriter(nomeArquivo1);
+                                                                                                        writer1.WriteLine($"Data: {DateTimeOffset.Now.ToString("dd/MM/yyyy HH:mm:ss")}" + " - " + $"ID Produto Prefat :  {pptf.ProdutoPrefat} - Prefaturamento: {pptf.Prefaturamento} - Erro: {etv.Message}");
+                                                                                                        writer1.Close();
+
+                                                                                                    }
+                                                                                                    //Verifica se o arquivo de Log já existe e inclui as informações.
+                                                                                                    else
+                                                                                                    {
+                                                                                                        using (StreamWriter sw = File.AppendText(path))
+                                                                                                        {
+                                                                                                            sw.WriteLine($"Data: {DateTimeOffset.Now.ToString("dd/MM/yyyy HH:mm:ss")}" + " - " + $"ID Produto Prefat :  {pptf.ProdutoPrefat} - Prefaturamento: {pptf.Prefaturamento} - Erro: {etv.Message}");
+                                                                                                        }
+                                                                                                    }
+
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                    catch (WebException epp)
+                                                                                    {
+                                                                                        Console.WriteLine(epp.Message);
+
+                                                                                        //Verificando a pasta de Log. 
+                                                                                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                                                                                        string path = @"C:\Dashboard-Log\" + DateTimeOffset.Now.ToString("ddMMyyyy") + ".log";
+
+                                                                                        //Verifica se o arquivo de Log não existe e inclui as informações.
+                                                                                        if (!File.Exists(path))
+                                                                                        {
+                                                                                            DirectoryInfo dir = new DirectoryInfo(FileLog);
+
+                                                                                            foreach (FileInfo fi in dir.GetFiles())
+                                                                                            {
+                                                                                                fi.Delete();
+                                                                                            }
+
+                                                                                            string nomeArquivo1 = @"C:\Dashboard-Log\" + DateTimeOffset.Now.ToString("ddMMyyyy") + ".log";
+                                                                                            StreamWriter writer1 = new StreamWriter(nomeArquivo1);
+                                                                                            writer1.WriteLine($"Data: {DateTimeOffset.Now.ToString("dd/MM/yyyy HH:mm:ss")}" + " - " + $"{epp.Message}");
+                                                                                            writer1.Close();
+
+                                                                                        }
+                                                                                        //Verifica se o arquivo de Log já existe e inclui as informações.
+                                                                                        else
+                                                                                        {
+                                                                                            using (StreamWriter sw = File.AppendText(path))
+                                                                                            {
+                                                                                                sw.WriteLine($"Data: {DateTimeOffset.Now.ToString("dd/MM/yyyy HH:mm:ss")}" + " - " + $"{epp.Message}");
+                                                                                            }
+                                                                                        }
+                                                                                    }
                                                                                 }
                                                                             }
                                                                             catch (WebException wpf)
